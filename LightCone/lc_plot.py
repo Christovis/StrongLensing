ab4c3293dc4ac88581837bdae4d9c0f6549e42bf
Christoph Becker <christoph.becker@durham.ac.uk>
2018-05-10 21:07:07 +0100

    Plot

--- LightCone/lc_plot.py
+++ LightCone/lc_plot.py
@@ -1,218 +1,200 @@
+from __future__ import division
+import sys
+import h5py
 import numpy as np
-import readsubf
-import readsnap
+from astropy import units as u
+from astropy import constants as const
+from astropy.cosmology import z_at_value
+from astropy.cosmology import Planck15 as cosmo
+from scipy.ndimage.filters import gaussian_filter
+from sklearn.neighbors import KDTree
 import matplotlib
-matplotlib.use('agg')
 import matplotlib.pyplot as plt
-
-
-def angle(v1, v2):
-	""" 
-	Returns the angle in radians between vectors 'v1' and 'v2'
-	"""
-	angle = np.arccos(np.dot(v1, v2)/(np.linalg.norm(v1)*np.linalg.norm(v2)))
-	return angle
-
-
-def check_in_sphere(centre, pos, Rth):
-	r = np.sqrt((pos[:, 0])**2 +
-				(pos[:, 1])**2 +
-				(pos[:, 2])**2)
-	indx = np.where(r < Rth)
-	return indx
-
+sys.path.append('/cosma5/data/dp004/dc-beck3')
+import readlensing as rf
 
 def rotate_axes(pos, av, bv, cv, x, y, z):
-	"""
-	Rotate coordinate axes
-	"""
-	R0 = np.outer(av, x)
-	R1 = np.outer(bv, y)
-	R2 = np.outer(cv, z)
-	R = np.asarray(R0 + R1 + R2)
-	pos = [np.dot(R, vec) for vec in pos]
-	return np.asarray(pos)
+    """
+    Rotate coordinate axes
+    """
+    R0 = np.outer(av, x)
+    R1 = np.outer(bv, y)
+    R2 = np.outer(cv, z)
+    R = np.asarray(R0 + R1 + R2)
+    pos = [np.dot(R.T, vec) for vec in pos]
+    return np.asarray(pos)
 
+def adaptively_smoothed_maps(X, h, M, bins, smooth_fac=0.5):
+    Ncells = bins
+    hbins = int(np.log2(h.max()/h.min()))+2
+    hbin_edges = 0.8*h.min()*2**np.arange(hbins)
+    hbin_mids = np.sqrt(hbin_edges[1:]*hbin_edges[:-1])
+    hmask = np.digitize(h,hbin_edges)-1
+    sigmaS = np.zeros((len(hbin_mids),Ncells[0],Ncells[1]))
+    for i in np.arange(len(hbin_mids)):
+        maskpos = X[hmask==i]
+        maskm = M[hmask==i]
+        maskSigma, xedges, yedges = np.histogram2d(maskpos[:, 0],
+                                                   maskpos[:, 1],
+                                                   bins=Ncells,
+                                                   #range=[[0.0, 5500],
+                                                   #       [-30, 30]],
+                                                   weights=maskm)
+        pixelsmooth = smooth_fac*hbin_mids[i]/(xedges[1]-xedges[0])
+        sigmaS[i] = gaussian_filter(maskSigma,pixelsmooth,truncate=3)
+    return np.sum(sigmaS,axis=0), xedges, yedges
 
-def check_in_ellipse(pos, a, b, c):
-	"""
-	centered on origin
-	a = semi-major axis
-	b = semi-intermediate axis
-	c = sami-minor axis
-	"""
-	r_xy = (pos[:, 0]**2/a**2) + (pos[:, 1]**2/b**2)
-	r_xz = (pos[:, 0]**2/a**2) + (pos[:, 2]**2/c**2)
-	r_yz = (pos[:, 1]**2/b**2) + (pos[:, 2]**2/c**2)
-	indx = np.where((r_xy <= 1.) & (r_xz <= 1.) & (r_yz <= 1.))
-	return indx
-
-
-def principal_axes(pos, s, q, centre):
-	"""
-	Reduced Moment of Inertia Tensor
-	Ellipsoid described by dimensionless parameters:
-	find shape in 10 iterations
-	minor-to-major axis ratio			s=c/a
-	intermediate-to-major axis ratio	q=b/a
-	minor-to-intermediate axis ratio	p=c/b
-	with a,b,c the principal axes
-
-	Input:
-	av, bv, cv, principal axes unit vectors
-	"""
-	x = pos[:, 0]
-	y = pos[:, 1]
-	z = pos[:, 2]
-	d2 = x*x + y*y/q**2 + z*z/s**2
-	Ixx = np.sum(x*x/d2)
-	Iyy = np.sum(y*y/d2)
-	Izz = np.sum(z*z/d2)
-	Ixy = np.sum(x*y/d2)
-	Ixz = np.sum(x*z/d2)
-	Iyz = np.sum(y*z/d2)
-	I = np.array([[Ixx, Ixy, Ixz],
-				  [Ixy, Iyy, Iyz],
-				  [Ixz, Iyz, Izz]])
-	evalue, evector = np.linalg.eigh(I)
-	return evalue, evector
-
-
-def ellipticity(subpos, submass, subrad, parpos):
-	"""
-	Based on Allgood et al. 2006 (arXiv:astro-ph/0508497)
-	Iteration stops if s&q have changed less than 1e-3 (arXiv:0705.2037),
-	or if enclosed regions contains less than 200 particles (arXive:1402.0903.pdf).
-
-	Input:
-		subpos: positions of subhalo centres
-		submass: mass of subhalos in solar masses
-		subrad: radius of subhalo e.g. R_vir, R_vmax, R_halfmass, ...
-
-	Output:
-		sub_e: subhalo ellipticity along eigenvectors of reduced momentum of
-		 	   inertia tensor
-		sub_av, sub_bv, sub_cv: normalized eigenvectors of reduced momentum of
-								inertia tensor
-	"""
-	sub_e = np.zeros((len(submass), 3))
-	sub_av = np.zeros((len(submass), 3))
-	sub_bv = np.zeros((len(submass), 3))
-	sub_cv = np.zeros((len(submass), 3))
-	subindx = np.where(submass > 10**11)
-	subpos = subpos[subindx]
-	subrad = subrad[subindx]
-	submass = submass[subindx]
-
-	# Iterate through Subhalos
-	for i in range(0, len(submass)):
-		centre = subpos[i]
-		# Centre particles on coord. origin
-		par_in_sub = np.asarray(parpos - centre)
-		radius = 9*subrad[i]
-		indx = check_in_sphere(centre, par_in_sub, radius)
-		par_in_sub = par_in_sub[indx]
-		# Axis ratios
-		[s, q] = [1, 1]
-		axes_m, axes_v = principal_axes(par_in_sub, s, q, centre)
-		[c, b, a] = [np.sqrt(axes_m[0]), np.sqrt(axes_m[1]), np.sqrt(axes_m[2])]
-		axes_ell = [radius, radius, radius]
-		# Normalized principal axis
-		cv = axes_v[0]/np.linalg.norm(axes_v[0])
-		bv = axes_v[1]/np.linalg.norm(axes_v[1])
-		av = axes_v[2]/np.linalg.norm(axes_v[2])
-		# Rotate Coord. to new coord. frame
-		par_in_sub = rotate_axes(par_in_sub, av, bv, cv,
-								[1, 0, 0], [0, 1, 0], [0, 0, 1])
-		while ((s-c/a >= 1e-3) and (q-b/a >= 1e-3)) and (len(indx[0]) >= 200):
-			[s, q] = [c/a, b/a]
-			axes_ell[0] *= 1
-			axes_ell[1] = 2*subhmrad[i]*q
-			axes_ell[2] = 2*subhmrad[i]*s
-			indx = check_in_ellipse(par_in_sub, axes_ell[0],
-									axes_ell[1], axes_ell[2])
-			par_in_sub = par_in_sub[indx]
-			axes_m, axes_v = principal_axes(par_in_sub, s, q, centre)
-			[c, b, a] = [np.sqrt(axes_m[0]), np.sqrt(axes_m[1]), np.sqrt(axes_m[2])]
-		
-		if len(indx[0]) >= 200:
-			[s, q, p] = [c/a, b/a, c/b]
-			sub_e[subindx[i], 0] = 1 - s  # (a - b)/(a + b)
-			sub_e[subindx[i], 1] = 1 - q  # (a - c)/(a + b)
-			sub_e[subindx[i], 2] = 1 - p  # (b - c)/(b + c)
-			sub_va[subindx[i], 0] = av
-			sub_vb[subindx[i], 1] = bv
-			sub_vc[subindx[i], 2] = cv
+def projected_surface_density(X,M,bins=512,smooth=True,smooth_fac=None,neighbour_no=None):
+    Ncells = bins
+    
+    # Shift particle coordinates to centre
+    if smooth is True:
+        if smooth_fac is None:
+            print "Need to supply a value for smooth_fac when smooth=True"
+            return
+        if neighbour_no is None:
+            print "Need to supply a value for neighbour_no when smooth=True"
+            return
+        # Find 'smoothing lengths'
+        kdt = KDTree(X, leaf_size=30, metric='euclidean')
+        dist, ids = kdt.query(X, k=neighbour_no, return_distance=True)
+        h = np.max(dist,axis=1)
+        mass_in_cells, xedges, yedges = adaptively_smoothed_maps(X, h, M,
+                                                                 bins=Ncells,
+                                                                 smooth_fac=smooth_fac)
+    else:
+        mass_in_cells, xedges, yedges = np.histogram2d(X[:,0], X[:,1],
+                                                       bins=[Ncells, Ncells],
+                                                       #range=[[0, 5500],
+                                                       #       [-30, 30]],
+                                                       weights=M)
+    dx, dy = xedges[1]-xedges[0], yedges[1]-yedges[0]
+    Sigma = mass_in_cells / (dx*dy)
+    xs = 0.5*(xedges[1:]+xedges[:-1])
+    ys = 0.5*(yedges[1:]+yedges[:-1])
+    return Sigma, xs, ys
 
 ###############################################################################
-# Load Simulation Header
-simdir = '/cosma6/data/dp004/dc-arno1/SZ_project/non_radiative_hydro/L62_N512_GR_pure_rerun'
-snapdir = simdir+'/snapdir_%03d/snap_%03d.0'
-header = readsnap.snapshot_header(snapdir % (45, 45))
+# Load Simulation Specifications
+LCSettings = '/cosma5/data/dp004/dc-beck3/shell_script/LCSettings.txt'
+dd, sim_phy, sim_name, dd, dd, lc_dir, dd, HQ_dir = rf.Simulation_Specs(LCSettings)
 
-subhalos = readsubf.subfind_catalog(simdir, 45, masstab=True)
-subpos = subhalos.sub_pos
-submass = subhalos.sub_mass*1e10/header.hubble
-subhmrad = subhalos.sub_halfmassrad
-subvmaxrad = subhalos.sub_vmaxrad
-snapdir = simdir+'/snapdir_%03d/snap_%03d'
-parpos = readsnap.read_block(snapdir % (45, 45), 'POS ', parttype=0)
+###############################################################################
+# Load Simulated Lightcone with Subhalos and Supernovae
+lc_file = lc_dir[0]+'LC_'+sim_name[0]+'.h5'
+# Load LightCone Contents
+LC = h5py.File(lc_file, 'r')
 
-sub_e, sub_va, sub_vb, sub_vc = ellipticity(subpos, submass, subvmaxrad, parpos)
+lc_sn_file = lc_dir[0]+'LC_SN_'+sim_name[0]+'.h5'
+# Load LightCone Contents
+LC_SN = h5py.File(lc_sn_file, 'r')
+
+###############################################################################
+# Select Subhalos and Supernovae
+HaloPosLC = LC['position_lc'][::10]
+M200 = LC['Mvir'][::10]*0.6774/1e10
+print(len(M200))
+#sys.exit("Error message")
+
+indx = np.where((LC_SN['Halo_z'][:] < 0.4) & (0.3 < LC_SN['Halo_z'][:]))[0]
+Halo_indx = indx[-1]
+Halo_ID = LC_SN['Halo_ID'][:][Halo_indx]
+indx = np.where(LC_SN['Src_ID'][:] == Halo_ID)[0]
+SrcPosLC = LC_SN['SrcPosSky'][:][indx]
+Halo_unit1 = np.asarray(LC_SN['HaloPosLC'][:][Halo_indx, :]/ \
+                        np.linalg.norm(LC_SN['HaloPosLC'][:][Halo_indx, :]))
+Halo_unit2 = [-Halo_unit1[1], Halo_unit1[0], 0]
+Halo_unit3 = np.cross(Halo_unit1, Halo_unit2)
+SrcPosLC1 = rotate_axes(SrcPosLC, [1, 0, 0], [0, 1, 0], [0, 0, 1],
+                     Halo_unit1, Halo_unit2, Halo_unit3)
 
 
-# [a, b, c] = [3, 1, 0.5]
-# theta = [30, 30, 30]
-indx = check_in_sphere(subpos[0], parpos, a)
-parpos = parpos[indx]
-plt.scatter(parpos[:, 0], parpos[:, 1], marker='.', c='k')
-plt.savefig('sphere.png', bbox_inches='tight')
-plt.clf()
+indx = np.where((LC_SN['Halo_z'][:] < 0.2) & (0.1 < LC_SN['Halo_z'][:]))[0]
+Halo_indx = indx[-1]
+Halo_ID = LC_SN['Halo_ID'][:][Halo_indx]
+indx = np.where(LC_SN['Src_ID'][:] == Halo_ID)[0]
+SrcPosLC = LC_SN['SrcPosSky'][:][indx]
+Halo_unit1 = np.asarray(LC_SN['HaloPosLC'][:][Halo_indx, :]/ \
+                        np.linalg.norm(LC_SN['HaloPosLC'][:][Halo_indx, :]))
+Halo_unit2 = [-Halo_unit1[1], Halo_unit1[0], 0]
+Halo_unit3 = np.cross(Halo_unit1, Halo_unit2)
+SrcPosLC2 = rotate_axes(SrcPosLC, [1, 0, 0], [0, 1, 0], [0, 0, 1],
+                     Halo_unit1, Halo_unit2, Halo_unit3)
 
-indx = check_in_ellipse(subpos[0], parpos, a, b, c, theta)
-parpos = parpos[indx]
-plt.xlim(-1.5, 1.5)
-plt.ylim(-1.5, 1.5)
-plt.scatter(parpos[:, 1], parpos[:, 2], marker='.', c='k')
-plt.savefig('ellipse.png', bbox_inches='tight')
-plt.clf()
+indx = np.where((LC_SN['Halo_z'][:] < 0.8) & (0.7 < LC_SN['Halo_z'][:]))[0]
+Halo_indx = indx[-1]
+Halo_ID = LC_SN['Halo_ID'][:][Halo_indx]
+indx = np.where(LC_SN['Src_ID'][:] == Halo_ID)[0]
+SrcPosLC = LC_SN['SrcPosSky'][:][indx]
+Halo_unit1 = np.asarray(LC_SN['HaloPosLC'][:][Halo_indx, :]/ \
+                        np.linalg.norm(LC_SN['HaloPosLC'][:][Halo_indx, :]))
+Halo_unit2 = [-Halo_unit1[1], Halo_unit1[0], 0]
+Halo_unit3 = np.cross(Halo_unit1, Halo_unit2)
+SrcPosLC3 = rotate_axes(SrcPosLC, [1, 0, 0], [0, 1, 0], [0, 0, 1],
+                     Halo_unit1, Halo_unit2, Halo_unit3)
+###############################################################################
+print(' Smooth distribution')
+Ncells = [int(round(11/15*1000)), int(round(4/15*1000))]
+smooth_fac = 0.5
 
+Sigma, x, y = projected_surface_density(HaloPosLC, M200, Ncells, smooth=True,
+                                        smooth_fac=0.8, neighbour_no=4)
 
-# subpos = rotation_s(subpos)
-## PLOT ##
-plt.figure(1)
-plt.scatter(subpos[:, 0], subpos[:, 1], marker='.', c='k')
-plt.savefig('SimBox_Visual_xy.png', bbox_inches='tight')
-plt.clf()
+print(np.max(Sigma), np.mean(Sigma))
+###############################################################################
+# PLOT 
+xticks_Mpc = np.array([1, 2, 3, 4, 5])*1e3*u.Mpc
+z = np.array([0.0, 0.5, 1, 1.5, 2])
+xticks_z = cosmo.comoving_distance(z).to_value('Mpc')
 
-plt.figure(2)
-plt.scatter(subpos[:, 1], subpos[:, 2], marker='.', c='k')
-plt.savefig('SimBox_Visual_yz.png', bbox_inches='tight')
-plt.clf()
+fig = plt.figure(figsize=(15,4))
+ax1 = fig.add_subplot(111)
+ax1.imshow((Sigma).T, #extent=[x.min(), x.max(), y.min(), y.max()],
+           cmap='jet', origin='lower')
+#ax1.scatter(HaloPosLC[:, 0], HaloPosLC[:, 1], marker='.', s=1, c='w')
+#ax1.scatter(SrcPosLC1[:, 0], SrcPosLC1[:, 1], marker='.', s=1, c='r')
+#ax1.scatter(SrcPosLC2[:, 0], SrcPosLC2[:, 1], marker='.', s=1, c='r')
+#ax1.scatter(SrcPosLC3[:, 0], SrcPosLC3[:, 1], marker='.', s=1, c='r')
+#ax1.set_facecolor('k')
+#ax1.set_xlim(0, 5500)
+#ax1.set_ylim(-30, 30)
+#ax2 = ax1.twiny()
+#ax2.set_xticks(xticks_z)
+#ax2.set_xticklabels(['{:g}'.format(redshift) for redshift in z]);
+ax1.set_xlabel('Comoving Distance [Mpc]')
+ax1.set_ylabel('Comoving Distance [Mpc]')
+#ax2.set_xlabel('Redshift')
+fig.savefig('Lightcone_xy.png', bbox_inches='tight')
+fig.clf()
+
+#fig = plt.figure(figsize=(15,4))
+#ax1 = fig.add_subplot(111)
+#ax1.scatter(HaloPosLC[:, 0], HaloPosLC[:, 2], marker='.', s=1, c='w')
+#ax1.scatter(SrcPosLC1[:, 0], SrcPosLC1[:, 2], marker='.', s=1, c='r')
+#ax1.scatter(SrcPosLC2[:, 0], SrcPosLC2[:, 2], marker='.', s=1, c='r')
+#ax1.scatter(SrcPosLC3[:, 0], SrcPosLC3[:, 2], marker='.', s=1, c='r')
+#ax1.set_facecolor('k')
+#ax1.set_xlim(0, 5500)
+#ax1.set_ylim(-40, 40)
+#ax2 = ax1.twiny()
+#ax2.set_xticks(xticks_z)
+#ax2.set_xticklabels(['{:g}'.format(redshift) for redshift in z]);
+#ax1.set_xlabel('Comoving Distance [Mpc]')
+#ax1.set_ylabel('Comoving Distance [Mpc]')
+#ax2.set_xlabel('Redshift')
+#fig.savefig('Lightcone_xz.png', bbox_inches='tight')
+#fig.clf()
 #
-plt.figure(3)
-plt.scatter(subpos[:, 0], subpos[:, 2], marker='.', c='k')
-plt.savefig('SimBox_Visual_xz.png', bbox_inches='tight')
-plt.clf()
-#PLOT
-#plt.gca().set_aspect('equal', adjustable='box')
-#plt.scatter(par_in_sub[::10, 0], par_in_sub[::10, 1], marker='.', c='k')
-#plt.plot([0, av[0]], [0, av[1]], 'r')
-#plt.plot([0, bv[0]], [0, bv[1]], 'r')
-#plt.plot([0, cv[0]], [0, cv[1]], 'r')
-#plt.savefig('ellipse_%02d_xy.png' % k, bbox_inches='tight')
-#plt.clf()
-#plt.gca().set_aspect('equal', adjustable='box')
-#plt.scatter(par_in_sub[::10, 0], par_in_sub[::10, 1], marker='.', c='k')
-#plt.plot([0, av[2]], [0, av[1]], 'r')
-#plt.plot([0, bv[2]], [0, bv[1]], 'r')
-#plt.plot([0, cv[2]], [0, cv[1]], 'r')
-#plt.savefig('ellipse_%02d_zy.png' % k, bbox_inches='tight')
-#plt.clf()
-#plt.gca().set_aspect('equal', adjustable='box')
-#plt.scatter(par_in_sub[:: 10, 0], par_in_sub[::10, 1], marker='.', c='k')
-#plt.plot([0, av[2]], [0, av[0]], 'r')
-#plt.plot([0, bv[2]], [0, bv[0]], 'r')
-#plt.plot([0, cv[2]], [0, cv[0]], 'r')
-#plt.savefig('ellipse_%02d_zx.png' % k, bbox_inches='tight')
-#plt.clf()
+#fig = plt.figure(figsize=(4,4))
+#ax1 = fig.add_subplot(111)
+#ax1.scatter(HaloPosLC[:, 1], HaloPosLC[:, 2], marker='.', s=1, c='w')
+#ax1.scatter(SrcPosLC1[:, 1], SrcPosLC1[:, 2], marker='.', s=1, c='r')
+#ax1.scatter(SrcPosLC2[:, 1], SrcPosLC2[:, 2], marker='.', s=1, c='r')
+#ax1.scatter(SrcPosLC3[:, 1], SrcPosLC3[:, 2], marker='.', s=1, c='r')
+#ax1.set_facecolor('k')
+#ax1.set_xlim(-30, 30)
+#ax1.set_ylim(-30, 30)
+#ax1.set_xlabel('Comoving Distance [Mpc]')
+#ax1.set_ylabel('Comoving Distance [Mpc]')
+#fig.savefig('Lightcone_yz.png', bbox_inches='tight')
+#fig.clf()
