43efa2619bed06b257a72a03ef91910ff2eb78e9
Christoph Becker <christoph.becker@durham.ac.uk>
2018-08-28 11:15:23 +0100

    ctypes(not much better) & abs. magnitudes

--- LightCone/LC_SNeIa.py
+++ LightCone/LC_SNeIa.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys, logging
+import time
 import numpy as np
 import random as rnd
 import astropy
@@ -12,6 +13,7 @@ import h5py
 import CosmoDist as cd
 #from Supernovae import SN_Type_Ia as SN
 import lc_supernovae as SN
+import cfuncs as cf
 sys.path.insert(0, '/cosma5/data/dp004/dc-beck3/')
 import readsnap
 import readlensing as rf
@@ -26,22 +28,25 @@ logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s',
 
 ###############################################################################
 # Load Simulation Specifications
-LCSettings = '/cosma5/data/dp004/dc-beck3/shell_script/LCSettings.txt'
+LCSettings = '/cosma5/data/dp004/dc-beck3/StrongLensing/shell_script/LCSettings.txt'
 sim_dir, sim_phy, sim_name, sim_col, hf_dir, hf_name, lc_dir, dd, HQ_dir = rf.Simulation_Specs(LCSettings)
 
 ###############################################################################
 # Define Supernova Distribution Parameters
-c = const.c.to_value('km/s')
-# Region around Lense...
+try:
+    c = float((const.c).to('km/s')*(u.second/u.km))
+except:
+    c = (const.c).to_value('km/s')
+assert type(c) == float, "Incorrect type of c"
 # Time interval
-years = 1e5  # [yr]
+years = 1e3  # [yr]
 
 ###############################################################################
 # Iterate through Simulations
 for sim in range(len(sim_dir)):
     logging.info('Seed SNIa in Light-cone for: %s' % sim_name[sim])
     # LightCone file for lens properties
-    lc_file = lc_dir[sim]+hf_name+'/LC_'+sim_name[sim]+'1.h5'
+    lc_file = lc_dir[sim]+hf_name+'/LC_'+sim_name[sim]+'_2.h5'
     # Simulation Snapshots
     snapfile = sim_dir[sim]+'snapdir_%03d/snap_%03d'
 
@@ -54,7 +59,6 @@ for sim in range(len(sim_dir)):
     LC = rf.LightCone_without_SN(lc_file, hf_name)
     # Select Halos
     LC = SN.select_halos(LC, hf_name)
-    print('Number of Halos: ', len(LC['M200']))
 
     # convert conformal time to redshift
     cosmosim = {'omega_M_0' : 0.3089,
@@ -65,16 +69,22 @@ for sim in range(len(sim_dir)):
 
     # Redshift bins for SNeIa
     zr = np.linspace(0, 2, 1000)
-    dist_zr = cosmo.comoving_distance(zr).to_value('Mpc')
+    try:
+        dist_zr = ((cosmo.comoving_distance(zr)).to('Mpc')*1/u.Mpc).astype(np.float)
+        dist_zr = [float(xx) for xx in dist_zr]  # convert Quantity to np.list(float)
+    except:
+        dist_zr = (cosmo.comoving_distance(zr)).to_value('Mpc')
+    assert type(dist_zr) == list, "Incorrect type of dist_zr"
     # middle redshift & distnace
     zmid = np.linspace((zr[1]-zr[0])/2, zr[-2] + (zr[-1]-zr[-2])/2, 999)
-    #dist_mid = cosmo.comoving_distance(zmid).to_value('Mpc')
     # Comoving distance between redshifts
     dist_bet = [cd.comoving_distance(zr[j+1],zr[j],**cosmosim) for j in range(len(zr)-1)]#[Mpc]
-    
-    # Initialize Lists to save
-    L_indx=[]; L_fov=[]; S_ID=[]; S_z=[]; S_possky=[]; L_AE=[]; R_E=[]
 
+    # Initialize Lists to save
+    L_indx=[]; L_fov_arcsec=[]; L_fov_Mpc=[]
+    S_ID=[]; S_z=[]; S_possky=[]; L_AE=[]; R_E=[]
+
+    S_ID=np.zeros((1)); S_z=np.zeros((1)); S_skypos=np.zeros((1,3))
     # Iterate over lenses
     for i in range(len(LC['Halo_ID'])):
         # Lens properties
@@ -86,110 +96,111 @@ for sim in range(len(sim_dir)):
         # Find starting redshift of Box
         indx = np.where(zr > LC['redshift'][i])[0]
         dist_sr = [dist_zr[idx] for idx in indx]
-        #distmid_p = [dist_mid[idx] for idx in indx[:-2]]
         distbet_p = [dist_bet[idx] for idx in indx[:-1]]
         # Calculate app. Einstein radius for point lense
-        A_E = SN.Einstein_ring(LC['VelDisp'][i]+20, c, LC['redshift'][i],
-                               lensdist, None, 'rad')
-        u_lenspos = np.asarray(l_poslc/np.linalg.norm(l_poslc))
-        l_lenspos = np.linalg.norm(l_poslc)
+        if hf_name == 'Subfind':
+            A_E = SN.Einstein_ring(LC['VelDisp'][i]+10, c, LC['redshift'][i],
+                                   lensdist, None, 'rad')
+            u_lenspos = np.asarray(l_poslc/np.linalg.norm(l_poslc))
+            l_lenspos = np.linalg.norm(l_poslc)
 
-        # Calculate Volume for SNeIa distribution
-        #fov_rad = A_E*2 # for Rockstar
-        fov_rad = A_E   # for Subfind 
-        V, fov_Mpc = SN.Einstein_Volume(fov_rad, dist_sr, distbet_p)
+            # Calculate Volume for SNeIa distribution
+            fov_rad = A_E*0.5
+            V, fov_Mpc = SN.Einstein_Volume(fov_rad, dist_sr, distbet_p)
+        elif hf_name == 'Rockstar':
+            A_E = SN.Einstein_ring(LC['VelDisp'][i], c, LC['redshift'][i],
+                                   None, None)
+            u_lenspos = np.asarray(l_poslc/np.linalg.norm(l_poslc))
+            l_lenspos = np.linalg.norm(l_poslc)
 
+            # Calculate Volume for SNeIa distribution
+            fov_rad = A_E  #0.5
+            V, fov_Mpc = SN.Einstein_Volume(fov_rad, dist_sr, distbet_p)
+        
         # Number of SNeIa in time and Volume
-        SNIa_num = SN.SNeIa_distr(years, V, zmid, agefunc, redfunc, **cosmosim)
+        SNIa_num = SN.SNIa_distr(years, V, zmid, agefunc, redfunc, **cosmosim)
         [dec, SNIa_num] = np.modf(SNIa_num)
         rand = rnd.random()
         indx = np.where(dec > rnd.random())
         SNIa_num[indx] += 1
 
-        # initilize variable to see if lens has supernova source
-        checksrc = 0
-        # Find Volums wih Supernovae
+        if np.sum(SNIa_num) == 0.0:
+            continue
+        else:
+            pass
+
+        # Position SNIa in Light-Cone
+        ## Find Volums wih Supernovae
         indx = np.nonzero(SNIa_num)
-        # Iterate over Volume
-        for y in indx[0]:
-            [dist_min, dist_max] = [dist_sr[y], dist_sr[y+1]]
-            # Iterate over Supernovae
-            for x in range(int(SNIa_num[y])):
-                # Generate random SNeIa location within a cone
-                # with apex-angle thetaE_inf along the l.o.s.
-                radial_rnd = u_lenspos*(l_lenspos + \
-                             rnd.random()*(dist_max-l_lenspos))  # [Mpc]
-                srcdist = np.sqrt(radial_rnd[0]**2 + radial_rnd[1]**2 + radial_rnd[2]**2)
-                zs = z_at_value(cosmo.comoving_distance, srcdist*u.Mpc, zmax=2)
-                # max. distance equa to re
-                sposx = srcdist  #[Mpc]
-                charge = 1 if rnd.random() < 0.5 else -1
-                sposy = charge*rnd.random()*fov_Mpc[y]  #[Mpc]
-                charge = 1 if rnd.random() < 0.5 else -1
-                sposz = charge*rnd.random()*fov_Mpc[y]  #[Mpc]
-                # Calculate Einstein Radius for Source
-                Ds = cosmo.angular_diameter_distance(zs)  #[Mpc]
-                Dls = cosmo.angular_diameter_distance_z1z2(zl, zs)  #[Mpc]
-                # only valid for point lense... not accurate
-                #[thetaE, radiusE] = SN.Einstein_ring(LC['VelDisp'][i],
-                #                     c, zl, Dls, Ds, 'kpc')  #[rad],[kpc]
-                # Write glafic file
-                S_ID.append(i);         S_z.append(zs)
-                S_possky.append([sposx, sposy, sposz])
-                #A_E.append(A_E)#;     R_E.append(radiusE)
-            checksrc = 1
-        if checksrc == 1:
+        ## Place SNIa in Light-Cone
+        [sid, sx, sy, sz] = cf.call_seed_snia(i, indx, SNIa_num, dist_sr,
+                                               u_lenspos, l_lenspos, fov_Mpc)
+        ## most time consuming
+        sred = [z_at_value(cosmo.comoving_distance, xx*u.Mpc, zmax=2) for xx in sx]
+        S_ID = np.concatenate((S_ID, sid), axis=None)
+        S_z = np.concatenate((S_z, sred), axis=None)
+        sskypos = np.stack((sx, sy, sz)).transpose()
+        S_skypos = np.concatenate((S_skypos, sskypos))
+
+
+        # SNIa abs. magnitude distribution
+        SN.SNIa_magnitudes(SNIa_num)
+        M_SNIa = np.random.normal(M_SNIa, sigma_SNIa, SNIa_num)
+
+        if np.count_nonzero(SNIa_num) != 0:
             # Write glafic file
-            fov = (fov_rad*u.rad).to_value('arcsec')  # convert radians to arcsec
-            A_E = (A_E*u.rad).to_value('arcsec')  # convert radians to arcsec
-            L_indx.append(i);   L_fov.append(fov); L_AE.append(A_E)
+            try:
+                fov_arcsec = (fov_rad*u.rad).to_value('arcsec')  # radians to arcsec
+                A_E = (A_E*u.rad).to_value('arcsec')  # convert radians to arcsec
+            except:
+                fov_arcsec = float((fov_rad*u.rad).to('arcsec')*1/u.arcsec)
+                A_E = float((A_E*u.rad).to('arcsec')*1/u.arcsec)
+            assert type(fov_arcsec) == float, "Incorrect type of fov_arcsec"
+            assert type(A_E) == float, "Incorrect type of A_E"
+            L_indx.append(i); L_fov_arcsec.append(fov_arcsec);
+            L_fov_Mpc.append(fov_Mpc[0]); L_AE.append(A_E)
     
-    S_ID = np.asarray(S_ID);       S_z = np.asarray(S_z)
-    S_possky = np.asarray(S_possky)
-    L_AE = np.asarray(L_AE);         R_E = np.asarray(R_E)
-    L_indx = np.asarray(L_indx);   L_fov = np.asarray(L_fov)
-    print('->', len(L_indx), 'number of lenses')
-    hf = h5py.File(lc_dir[sim]+hf_name+'/LC_SN_'+sim_name[sim]+'_rndseed1.h5', 'w')
+    S_ID = np.asarray(S_ID); S_z = np.asarray(S_z)
+    S_skypos = np.asarray(S_skypos)
+    L_AE = np.asarray(L_AE); R_E = np.asarray(R_E)
+    L_indx = np.asarray(L_indx); 
+    #L_fov_arcsec = np.asarray(L_fov_arcsec)
+    #L_fov_Mpc = np.asarray(L_fov_Mpc)
+    L_fov_arcsec = [float(xx) for xx in L_fov_arcsec]
+    L_fov_Mpc = [float(xx) for xx in L_fov_Mpc]
+    hf = h5py.File(lc_dir[sim]+hf_name+'/LC_SN_'+sim_name[sim]+'_rndseed31.h5', 'w')
     if hf_name == 'Subfind':
         hf.create_dataset('Halo_Subfind_ID', data=LC['Halo_ID'][L_indx])  # Rockstar ID
-        hf.create_dataset('Halo_ID', data=L_indx)  # not Rockstar ID
+        hf.create_dataset('Halo_ID', data=L_indx)  # Light-Cone ID
         hf.create_dataset('snapnum', data=LC['snapnum'][L_indx])
         hf.create_dataset('Halo_z', data=LC['redshift'][L_indx])
-        hf.create_dataset('M200', data=LC['M200'][L_indx])
-        hf.create_dataset('Rhalfmass', data=LC['Rhalfmass'][L_indx])
-        hf.create_dataset('Rvmax', data=LC['Rvmax'][L_indx])
-        hf.create_dataset('Vmax', data=LC['Vmax'][L_indx])
         hf.create_dataset('HaloPosBox', data=LC['HaloPosBox'][L_indx])
         hf.create_dataset('HaloPosLC', data=LC['HaloPosLC'][L_indx])
-        hf.create_dataset('HaloVel', data=LC['HaloVel'][L_indx])
-        hf.create_dataset('VelDisp', data=LC['VelDisp'][L_indx])
-        hf.create_dataset('FOV', data=L_fov)
+        hf.create_dataset('FOV', data=[L_fov_arcsec, L_fov_Mpc])
         hf.create_dataset('Src_ID', data=S_ID)
         hf.create_dataset('Src_z', data=S_z)
-        hf.create_dataset('SrcPosSky', data=S_possky)
-        #hf.create_dataset('SrcPosBox', data=S_posbos)
-        hf.create_dataset('Einstein_angle', data=L_AE)  #[rad]
-        #hf.create_dataset('Einstein_radius', data=R_E)
+        hf.create_dataset('SrcPosSky', data=S_skypos)
+        hf.create_dataset('Einstein_angle', data=L_AE)  #[arcsec]
     elif hf_name == 'Rockstar':
+        hf.create_dataset('snapnum', data=LC['snapnum'][L_indx])
         hf.create_dataset('Halo_Rockstar_ID', data=LC['Halo_ID'][L_indx])  # Rockstar ID
         hf.create_dataset('Halo_ID', data=L_indx)  # not Rockstar ID
-        hf.create_dataset('snapnum', data=LC['snapnum'][L_indx])
         hf.create_dataset('Halo_z', data=LC['redshift'][L_indx])
-        hf.create_dataset('M200', data=LC['M200'][L_indx])
+        #hf.create_dataset('M200', data=LC['M200'][L_indx])
         hf.create_dataset('Rvir', data=LC['Rvir'][L_indx])
-        hf.create_dataset('Rsca', data=LC['Rsca'][L_indx])
-        hf.create_dataset('Rvmax', data=LC['Rvmax'][L_indx])
-        hf.create_dataset('Vmax', data=LC['Vmax'][L_indx])
+        #hf.create_dataset('Rsca', data=LC['Rsca'][L_indx])
+        #hf.create_dataset('Rvmax', data=LC['Rvmax'][L_indx])
+        #hf.create_dataset('Vmax', data=LC['Vmax'][L_indx])
         hf.create_dataset('HaloPosBox', data=LC['HaloPosBox'][L_indx])
         hf.create_dataset('HaloPosLC', data=LC['HaloPosLC'][L_indx])
-        hf.create_dataset('HaloVel', data=LC['HaloVel'][L_indx])
-        hf.create_dataset('VelDisp', data=LC['VelDisp'][L_indx])
-        hf.create_dataset('Ellip', data=LC['Ellip'][L_indx])
-        hf.create_dataset('Pa', data=LC['Pa'][L_indx])
-        hf.create_dataset('FOV', data=L_fov)
+        #hf.create_dataset('HaloVel', data=LC['HaloVel'][L_indx])
+        #hf.create_dataset('VelDisp', data=LC['VelDisp'][L_indx])
+        #hf.create_dataset('Ellip', data=LC['Ellip'][L_indx])
+        #hf.create_dataset('Pa', data=LC['Pa'][L_indx])
+        hf.create_dataset('FOV', data=[L_fov_arcsec, L_fov_Mpc])
         hf.create_dataset('Src_ID', data=S_ID)
         hf.create_dataset('Src_z', data=S_z)
-        hf.create_dataset('SrcPosSky', data=S_possky)
+        hf.create_dataset('SrcPosSky', data=S_skypos)
         #hf.create_dataset('SrcPosBox', data=S_posbos)
         hf.create_dataset('Einstein_angle', data=L_AE)  #[rad]
         #hf.create_dataset('Einstein_radius', data=R_E)
